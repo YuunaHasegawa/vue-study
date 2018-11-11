8fd78d2ade33a026e416378f26c760cc6c6c9f75
長谷川有那 <hasegawayuuna@yuna.local>
2018-11-11 20:19:43 +0900

    vue勉強

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Well come to Your Vue.js App" num="100" btn_text="test"/>
   </div>
 </template>
 
